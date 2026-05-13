744f132e77a8498edc6c432f1da59ff6342e2537
madhushalini23 <gundlurumadhushalini23@gmail.com>
2026-05-13 16:18:58 +0530

    conversation added

---
+++ 02-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "rahim:: hi robert"
+echo "robert:: hello rahim"
+echo "rahim:: how are you doing?"
+echo "robert:: i am doing good, how about you?"
+echo "rahim:: i am doing good as well"
