b24df7f3bd1b12a22624ed4189ffb29b8802bb26
madhushalini23 <gundlurumadhushalini23@gmail.com>
2026-05-13 16:39:50 +0530

    conversation added

--- 03-variables.sh
+++ 03-variables.sh
@@ -3,8 +3,8 @@
 PERSON1=Ramesh
 PERSON2=Suresh
 
-echo "PERSON1:: hi PERSON2"
-echo "PERSON2:: hello PERSON1"
-echo "PERSON1:: how are you doing?"
-echo "PERSON2:: i am doing good, how about you?"
-echo "PERSON1:: i am doing good as well"
+echo "$PERSON1:: hi $PERSON2"
+echo "$PERSON2:: hello $PERSON1"
+echo "$PERSON1:: how are you doing?"
+echo "$PERSON2:: i am doing good, how about you?"
+echo "$PERSON1:: i am doing good as well"
