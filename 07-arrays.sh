10f0a58d692d6ea5653c5b61bc3375632f9cfd00
madhushalini23 <gundlurumadhushalini23@gmail.com>
2026-05-14 09:49:43 +0530

    script added

---
+++ 07-arrays.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+MOVIES=("PUSHPA" "KGF" "RRR")
+# index starts from 0, size of is 3
+
+echo "first movie: ${MOVIES[0]}"
+echo "second movie: ${MOVIES[1]}"
+echo "third movie: ${MOVIES[2]}"
+
+echo "all movies are: ${MOVIES[@]}"
+
