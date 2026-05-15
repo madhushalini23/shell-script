c93b5ee322769193349cc68735c6c630991e4fce
madhushalini23 <gundlurumadhushalini23@gmail.com>
2026-05-15 11:51:50 +0530

    script added

--- 15-loops.sh
+++ 15-loops.sh
@@ -28,23 +28,3 @@ then
     echo "ERROR:: you must have sudo access to execute this script"
     exit 1
 fi
-
-dnf list installed mysql &>>$LOG_FILE_NAME
-if [ $? -ne 0 ]
-then
-    dnf install mysql -y &>>$LOG_FILE_NAME
-    VALIDATE $? "installing mysql"
-else
-    echo -e "mysql is already... $Y installed" $N
-fi
-
-
-dnf list installed git &>>$LOG_FILE
-if [ $? -ne 0 ]
-then
-    dnf install git -y &>>$LOG_FILE
-    VALIDATE $? "installing git"
-else 
-    echo -e "git is already... $Y installed" $N
-fi
-
