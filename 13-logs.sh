a6a2066e04f326641c260507a657299ce8910c20
madhushalini23 <gundlurumadhushalini23@gmail.com>
2026-05-15 11:12:59 +0530

    script added

--- 13-logs.sh
+++ 13-logs.sh
@@ -19,7 +19,7 @@ VALIDATE(){
     fi
 }
 
-echo "script started executing at: $TIMESTAMP" &>>$LOG_FILE
+echo "script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
 
 if [ $USERID -ne 0 ]
 then
@@ -27,20 +27,20 @@ then
     exit 1
 fi
 
-dnf list installed mysql &>>$LOG_FILE
+dnf list installed mysql &>>$LOG_FILE_NAME
 if [ $? -ne 0 ]
 then
-    dnf install mysql -y &>>$LOG_FILE
+    dnf install mysql -y &>>$LOG_FILE_NAME
     VALIDATE $? "installing mysql"
 else
     echo -e "mysql is already... $Y installed"
 fi
 
 
-dnf list installed git &>>$LOG_FILE
+dnf list installed git &>>$LOG_FILE_NAME
 if [ $? -ne 0 ]
 then
-    dnf install git -y &>>$LOG_FILE
+    dnf install git -y &>>$LOG_FILE_NAME
     VALIDATE $? "installing git"
 else 
     echo -e "git is already... $Y installed"
