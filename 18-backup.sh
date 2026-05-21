a48aa8791a0ea8e191cef165de35b189edbe4c7b
madhushalini23 <gundlurumadhushalini23@gmail.com>
2026-05-21 10:35:58 +0530

    script added

--- 18-backup.sh
+++ 18-backup.sh
@@ -36,4 +36,16 @@ then
     USAGE
 fi
 
+if [ ! -d $SOURCE_DIR ]
+then
+    echo -e "$SOURCE_DIR does not exist...please check"
+    exit 1
+fi
+
+if [ ! -d $DEST_DIR ]
+then
+    echo -e "$DEST_DIR does not exist...please check"
+    exit 1
+fi
+
 echo "script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
