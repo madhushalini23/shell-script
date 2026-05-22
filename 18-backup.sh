91eaf91154683fc13702530599251c585040fe1d
madhushalini23 <gundlurumadhushalini23@gmail.com>
2026-05-22 09:51:27 +0530

    script added

--- 18-backup.sh
+++ 18-backup.sh
@@ -10,7 +10,7 @@ DEST_DIR=$2
 DAYS=${3:-14}
 
 LOGS_FOLDER="/home/ec2-user/shellscript-logs"
-LOG_FILE=$(echo $0| cut -d "." -f1 )
+LOG_FILE=$(echo $0 | awk -F "/" '{print $NF}' | cut -d "." -f1 )
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
